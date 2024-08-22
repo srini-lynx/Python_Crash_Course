7d268d09f8e261a6088aebb4fd6de3c5f1f14969
Srinivasa Nagaraja <sri.lynx@gmail.com>
2024-08-22 04:45:59 +0000

    Exercise_2-8 Completed

---
+++ exercise_2-8.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+###########################################
+#Write add,sub,mult and division operations in print statements to see the results. You should create four lines that 
+#look like this: print(5 + 3)
+
+print("------------------------------")
+print("Basic Maths ")
+print("------------------------------")
+print("5 + 3   :: ", 5 + 3)
+print("16 - 8  :: ", 16 - 8)
+print("4 * 2   :: ", 4 * 2)
+print("16 / 2  :: ", 16 / 2)
