51ae5e03932d390494a000347c74e0071d5d9d0b
Srinivasa Nagaraja <sri.lynx@gmail.com>
2024-08-22 20:57:54 +0000

    using string method on list elements simple example

---
+++ CH03/string_method.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+#####################################################
+#Example 2: List
+#String methods on a list of Strings such as upper,lower and title
+#####################################################
+names = [ 'lAkShmi dEvi','naGAraja','sRinIvasa','vArAlakshmi','dEEptha' ]
+
+print("--------------------------------------------------------")
+print("names list contains following content")
+print("--------------------------------------------------------")
+print(names)
+print("--------------------------------------------------------")
+print("After using Upper  method upper() on the first element of names list")
+print(names[0].upper())
+print("--------------------------------------------------------")
+print("After using title method title() on the first element of names list")
+print(names[0].title())
+print("--------------------------------------------------------")
+print("After using lower method lower() on the first element of names list")
+print(names[0].lower())
+print("Printing the names list now")
+print(names)
+print("--------------------------------------------------------")
