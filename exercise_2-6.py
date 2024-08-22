75a4ae2ea98d9baf42a1258fb40a2f4b5c20908a
Srinivasa Nagaraja <sri.lynx@gmail.com>
2024-08-22 03:43:50 +0000

    Exercise 2-6 Completed

---
+++ exercise_2-6.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#############################################
+#Repeat the Ex2-5, but this time store the famous person's name in a variable called famous_person.
+#Then compose your message and store it in a new variable called message. Print your message.
+#############################################
+famous_person = "Albert Einstein"
+message = "A person who never made a mistake never tried anything new"
+
+print(famous_person+" "+"once said, " + message)
