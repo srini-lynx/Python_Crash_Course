213feaa510a3fda7216a3ec95d37b478d9844847
Srinivasa Nagaraja <sri.lynx@gmail.com>
2024-08-22 20:38:28 +0000

    List simple example names.py

---
+++ CH03/names.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+#############################################
+#Example for a List Data Structure in Python
+#Family memebers names stored in a List
+############################################
+names = [ 'Lakshmi Devi','Nagaraja', 'Srinivasa', 'Varalakshmi', 'Deeptha' ]
+print("-----------------------------------------------")
+print("names list has :: ", names)
+print("-----------------------------------------------")
+print("Individual Index value from names[0] to names[4]")
+print("-----------------------------------------------")
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+print("-----------------------------------------------")
