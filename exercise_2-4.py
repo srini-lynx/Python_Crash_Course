c46b106dd4a9c67c3b5551d78ef94edeb2d94495
Srinivasa Nagaraja <sri.lynx@gmail.com>
2024-08-22 01:49:32 +0000

    Exercise 2-4 Completed

---
+++ exercise_2-4.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+########################
+#Store a person's name in a Variable, and then print that person's name in lowercase, uppercase and titlecase.
+########################
+pname = "Deeptha Srinivasa"
+print("------------------------------------")
+print("Person name :: "+ pname)
+print("------------------------------------")
+print("Person  name in Lower Case :: "+ pname.lower())
+print("Person name in Upper Case  :: "+ pname.upper())
+print("Person name in Title Case  :: "+ pname.title())
+print("------------------------------------")
