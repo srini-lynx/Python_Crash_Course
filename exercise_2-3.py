108558fec0764e26021ce7ac87ac87c67e789cb9
Srinivasa Nagaraja <sri.lynx@gmail.com>
2024-08-22 01:40:59 +0000

    Exercise 1 Completed

---
+++ exercise_2-3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+##########################
+# Store a person's name in a Variable and print a message
+# to that person. Your message should be simple, such as,
+# "Hello Eric, would you like to learn some Python Today?"
+##########################
+
+name = "Deeptha Srinivasa"
+
+print("Hello" + " " + name + ", " + "would you like to learn some Python Today?")
