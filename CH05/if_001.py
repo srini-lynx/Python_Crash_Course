5ef76c685b8b2f65c3fc7f590833a34d9c074bc5
Srinivasa Nagaraja <sri.lynx@gmail.com>
2024-10-06 19:53:37 +0000

    demo if_001.py completed

---
+++ CH05/if_001.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#################################################
+#Example for if statement use in python
+#Change age value to less than 18 and more than
+#18 to check which block of if is executed.
+#################################################
+age = 18
+
+if age >= 18:
+    print("Your Age :: ", age)
+    print("You are Eligible to Vote")
+    print("Have you registered for Vote ?")
+else:
+    print("Your Age :: ", age)
+    print("No, you are not Eligible to Vote")
+    print("Please wait for the Right Age as per Law !!!")
